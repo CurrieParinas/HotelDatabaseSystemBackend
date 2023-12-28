3de22ae325a1a38674d7b7f5564c8f55fecf094d
rsgomez1 <rsgomez1@up.edu.ph>
2023-12-29 03:51:20 +0800

    Service Room Type Controller

--- springBackend/src/main/java/miancurocho/springbackend/controller/ServiceRoomTypeController.java
+++ springBackend/src/main/java/miancurocho/springbackend/controller/ServiceRoomTypeController.java
@@ -1,4 +1,27 @@
 package miancurocho.springbackend.controller;
 
+import lombok.RequiredArgsConstructor;
+import miancurocho.springbackend.entities.ServiceRoomType;
+import miancurocho.springbackend.service.ServiceRoomTypeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class ServiceRoomTypeController {
+    private final ServiceRoomTypeService serviceRoomTypeService;
+    @GetMapping(path="/allServiceRoomTypes")
+    public List<ServiceRoomType> displayServiceRoomTypes(){return serviceRoomTypeService.getAllServiceRoomTypes();}
+
+    @GetMapping(path="/getServicePerRoomType/{roomType}")
+    public List<ServiceRoomType> getServicePerRoomType(@PathVariable Long roomType){return serviceRoomTypeService.getServicePerRoomType(roomType);}
+
+    @GetMapping(path="/getServiceRoomType/{roomType}/{serviceId}")
+    public ServiceRoomType getServiceRoomType(@PathVariable Long roomType, @PathVariable Long serviceId){return serviceRoomTypeService.getServiceRoomType(roomType, serviceId);}
+
+    @PostMapping(path= "/addServiceRoomType")
+    public @ResponseBody ServiceRoomType addServiceRoomType(@RequestBody ServiceRoomType serviceRoomTypeToAdd){
+        return serviceRoomTypeService.addServiceRoomType(serviceRoomTypeToAdd);
+    }
 }
