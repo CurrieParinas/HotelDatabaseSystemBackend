07a124c330ee199e0ab42819222300fcdd0eb0fc
rsgomez1 <rsgomez1@up.edu.ph>
2023-12-30 20:24:55 +0800

    Fixed show all available rooms

--- springBackend/src/main/java/miancurocho/springbackend/repository/RoomRepository.java
+++ springBackend/src/main/java/miancurocho/springbackend/repository/RoomRepository.java
@@ -11,7 +11,16 @@ public interface RoomRepository extends JpaRepository<Room,Long> {
     Room findByRoomNumber(Long roomNumber);
 
     @Query(
-            value = "SELECT R.ROOM_NUMBER, ROOM_NAME, DESCRIPTION, PRICE, MAX_GUESTS, BEDS, AREA FROM ROOM R LEFT JOIN ROOM_TYPE RT on RT.ROOM_TYPE = R.ROOM_TYPE LEFT JOIN CHARGE C ON R.ROOM_NUMBER = C.ROOM_NUMBER LEFT JOIN BRN B ON C.BRN_ID = B.BRN_ID WHERE B.STATUS IS NULL OR B.STATUS = 'CHECKED-OUT' GROUP BY R.ROOM_NUMBER, ROOM_NAME, DESCRIPTION, PRICE, MAX_GUESTS, BEDS, AREA",
+            value = "SELECT R.ROOM_NUMBER, ROOM_NAME, DESCRIPTION, PRICE, MAX_GUESTS, BEDS, AREA\n" +
+                    "FROM ROOM R LEFT JOIN ROOM_TYPE RT on R.ROOM_TYPE = RT.ROOM_TYPE\n" +
+                    "            LEFT JOIN CHARGE C ON R.ROOM_NUMBER = C.ROOM_NUMBER\n" +
+                    "            LEFT JOIN BRN B ON C.BRN_ID = B.BRN_ID\n" +
+                    "WHERE R.ROOM_NUMBER NOT IN (SELECT R.ROOM_NUMBER\n" +
+                    "                            FROM ROOM R LEFT JOIN CHARGE C ON R.ROOM_NUMBER = C.ROOM_NUMBER\n" +
+                    "                                        LEFT JOIN BRN B ON C.BRN_ID = B.BRN_ID\n" +
+                    "                            WHERE B.BRN_ID NOT IN (SELECT BRN_ID FROM BRN WHERE STATUS = 'CHECKED-OUT')\n" +
+                    "                            GROUP BY R.ROOM_NUMBER)\n" +
+                    "GROUP BY R.ROOM_NUMBER, ROOM_NAME, DESCRIPTION, PRICE, MAX_GUESTS, BEDS, AREA",
             nativeQuery = true
     )
     List<Map<String, Object>> findAllAvailableRooms();
